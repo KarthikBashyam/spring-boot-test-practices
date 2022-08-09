79ed54f10eb343bbb81b8404d642410cc25dec4a
Karthik Bashyam <karthikb4ps@gmail.com>
2022-08-08 23:15:27 -0400

    Initial commit

---
+++ src/main/java/com/example/springboottestpractices/SpringBootTestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestPracticesApplication.class, args);
+	}
+
+}
